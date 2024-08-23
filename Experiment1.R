28d4be1f9f472cc3fba1b460d73d5631d5c9969f
aditya sharma <sharmaaditya18012004@gmail.com>
2024-08-23 20:39:40 +0530

    Intro to R  and Experiment 1

---
+++ Experiment1.R
@@ -0,0 +1,32 @@
+# Creating variables and assigning values
+x <- 10
+y <- 5
+
+# Performing basic mathematical operations
+
+# Addition
+sum_result <- x + y
+
+# Subtraction
+diff_result <- x - y
+
+# Multiplication
+prod_result <- x * y
+
+# Division
+div_result <- x / y
+
+# Modulus (remainder of the division)
+mod_result <- x %% y
+
+# Exponentiation (x raised to the power of y)
+exp_result <- x ^ y
+
+# Printing the results
+print(paste("Sum:", sum_result))
+print(paste("Difference:", diff_result))
+print(paste("Product:", prod_result))
+print(paste("Division:", div_result))
+print(paste("Modulus:", mod_result))
+print(paste("Exponentiation:", exp_result))
+
